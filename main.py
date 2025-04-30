240559e6fa82031ed0d5ff0a237e56879540db82
henriqkova <henriqueka2005@gmail.com>
2025-04-30 09:45:29 -0300

    pré prova

--- main.py
+++ main.py
@@ -1,50 +1,40 @@
 from fastapi import FastAPI, Query, HTTPException
-from fastapi import HTTPException
 import pandas as pd
-import numpy as np 
+import numpy as np
 
 app = FastAPI()
 
-# Lista de colunas permitidas
-colunas_permitidas = [
-    "ID RH", "Genero", "Cidade"
-]
-
+colunas_permitidas = ["ID RH", "Genero", "Cidade"]
 limitar_colunas = True
-# mude para True se quiser limitar as colunas
 
 def carregar_dados():
     df = pd.read_excel("aham.xlsx")
     df = df.replace({np.nan: None})
     return df
 
-# Rota de paginação
 @app.get("/tabela_paginada")
 def tabela_paginada(pagina: int = Query(1, ge=1), tamanho_pagina: int = Query(50, ge=1)):
     try:
         df = carregar_dados()
 
-        # Filtra apenas as colunas desejadas
         if limitar_colunas:
             df_filtrado = df[colunas_permitidas]
         else:
             df_filtrado = df
 
-        # Faz o cálculo de início e fim
         inicio = (pagina - 1) * tamanho_pagina
-        fim = inicio + tamanho_pagina   
+        fim = inicio + tamanho_pagina
 
-        # Filtra o DataFrame
         dados_paginados = df_filtrado.iloc[inicio:fim]
 
-        # Se não tiver mais dados
         if dados_paginados.empty:
-            raise HTTPException(status_code=404, detail=f"A página {pagina} não existe na tabela!")
+            raise HTTPException(status_code=404,detail=f"Erro: a página {pagina} não existe na tabela.")
 
-        return { 
+        return {
             "pagina": pagina,
             "tamanho_pagina": tamanho_pagina,
-            "dados": dados_paginados.to_dict(orient="records")
+            "dados": dados_paginados.to_dict(orient="records")  
         }
+
     except Exception as e:
-        return {"Erro": str(e)}
+        raise HTTPException(status_code=500, detail=f"Erro interno: {str(e)}")
